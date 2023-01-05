fb1a3b535746202564cd7c448eb754fb55a2f58b
MandySethi <mandy.sethi@lightspeedhq.com>
2023-01-05 18:59:10 +1300

    Enabling CORS

--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -1,8 +1,17 @@
 import { Table, Bucket } from "@serverless-stack/resources";
 
 export function StorageStack({ stack, app }) {
-    //Create an S3 bucket
-  const bucket = new Bucket(stack,'Uploads')
+  //Create an S3 bucket
+  const bucket = new Bucket(stack, "Uploads", {
+    cors: [
+      {
+        maxAge: "1 day",
+        allowedOrigins: ["*"],
+        allowedHeaders: ["*"],
+        allowedMethods: ["GET", "PUT", "POST", "DELETE", "HEAD"],
+      },
+    ],
+  });
   // Create the DynamoDB table
   const table = new Table(stack, "Notes", {
     fields: {
@@ -14,6 +23,6 @@ export function StorageStack({ stack, app }) {
 
   return {
     table,
-    bucket
+    bucket,
   };
 }
