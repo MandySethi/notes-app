5c0af9a8e5f72b27f9d467c09d9419bd9b5490aa
MandySethi <mandy.sethi@lightspeedhq.com>
2023-03-14 19:47:44 +1300

    A lot of changes to the app - check PR

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,11 @@
 const config = {
   // Backend config
 
+  //Stripe Publishable Key
+  STRIPE_KEY:
+    "pk_test_51MM6guDvZfghXI96bUWRmsVa7ganYjvCIrzSKT5TiaENdIOS49jqfEEGLJD0hGc76vVCnzbRbWf9TkN0c77lt7Gp00DT6hT8e3",
+
+  MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     REGION: process.env.REACT_APP_REGION,
     BUCKET: process.env.REACT_APP_BUCKET,
